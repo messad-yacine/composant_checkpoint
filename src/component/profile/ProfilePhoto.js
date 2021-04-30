22d828d7b16af044f4458a56a9572e5ac855b8a5
messad yacine <me.yacine@gmail.com>
2021-04-30 12:39:05 +0100

    first commit

---
+++ src/component/profile/ProfilePhoto.js
@@ -0,0 +1,9 @@
+import React from "react";
+import image from "./Gateau.jpg";
+<div className="App" >
+            <img src={image} style={{width:"120px", height:"120px"}}></img>
+            </div>
+
+}
+export default App;
+
